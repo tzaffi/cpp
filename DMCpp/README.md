1148f09891280f79dff15b9cb6a4432ecd89a6f5
Zeph Grunschlag <zgrunschlag@gmail.com>
2016-07-30 22:38:07 -0400

    Created 'Discovering Modern C++' directory, created Douglas Adam's hello42 and CLion generated CMakeLists file as well as vcs.xml

---
+++ DMCpp/README.md
@@ -0,0 +1,3 @@
+# Just some exercises..
+taken from "Discovering Modern C++" by 
+Peter Gottschling
---
+++ DMCpp/hello42.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Doug Adams says that the answer is\n"
+	    << std::endl << 6 * 7 << std:: endl;
+  return 0;
+}
