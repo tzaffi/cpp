dd32d5d02b5370815aa04b043a8a19ed1b463b31
Zeph Grunschlag <zgrunschlag@gmail.com>
2016-11-22 14:53:25 -0500

    IsCompleteCycle fully working and tested

--- GTraining/IsCompleteCycle.cpp
+++ GTraining/IsCompleteCycle.cpp
@@ -13,17 +13,19 @@ bool IsCompleteCycle(const vector<long>& cArray){
   } else if (N == 1) {
     return true;
   }
-  const long begin = 0;
-  long next = begin;
+  cout << "next:\t0,\t";
+  long next = 0;
   for(size_t i=0; i<N; ++i)
   {
-    long next = (next + cArray[i]) % N;
+    next = (next + cArray[next]) % N;
     if(next < 0){
       next += N;
     }
-    if(next == begin)
+    cout << next << ",\t";
+    if(next == 0)
       return i == N-1;
   }
+  cout << endl;
   return false;
 }
 
@@ -42,11 +44,12 @@ int main(){
     {{1, 1, 1, 1}, true},
     {{1, 2, 3}, false},
     {{7}, true},
-    {{}, false}/*TODO:,
+    {{}, false},
     {{1, -1, 1, -1}, false},
     {{1, -2, 2}, false},
-    {{7}, true},
-    {{}, false}*/
+    {{1,2,3,4,5,6,7}, false},
+    {{1,2,3,5,5,-4,2}, false},
+    {{3,-2,2,-2,9,-5,4,-1}, true}
   };
   cout << boolalpha << endl << endl << "testCases:" << endl << testCases << endl;
 
